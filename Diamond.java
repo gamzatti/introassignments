1b6266c36c3ce4c0b51dc9ce3d2eb4e851e61464
Emily Coats <emilycoats@riseup.net>
2018-04-27 17:17:38 +1000

    Add diamond name method.

--- Diamond.java
+++ Diamond.java
@@ -1,8 +1,10 @@
 public class Diamond {
     public static void main(String [] args) {
         Diamond d = new Diamond();
-        d.drawDiamond(5);
+        //d.drawDiamond(3);
+        d.drawDiamondName(3);
         //d.drawIsoc(3);
+        //d.drawMiddle(3);
     }
     public void drawLine(int n, int i){
         for (int k=1; k<=(n-(i/2)); k++) {
@@ -18,9 +20,13 @@ public class Diamond {
     }
 
     public void drawIsoc(int n) {
-        for (int i=1; i<(n*2); i+=2) {
+        for (int i=1; i<(n*2)-2; i+=2) {
             drawLine(n, i);
         }
+    }
+
+    public void drawMiddle(int n) {
+        drawLine(n, n+2);
     }
 
     public void drawBottom(int n) {
@@ -30,6 +36,13 @@ public class Diamond {
     }
     public void drawDiamond(int n) {
         drawIsoc(n);
+        drawMiddle(n);
         drawBottom(n);
         }
+
+    public void drawDiamondName(int n) {
+        drawIsoc(n);
+        System.out.println("Emily");
+        drawBottom(n);
+    }
 }
