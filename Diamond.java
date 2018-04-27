c801e6b2b710962da829cd533918a92a5c878dfb
Emily Coats <emilycoats@riseup.net>
2018-04-27 17:09:36 +1000

    Refactor to separate long methods.

--- Diamond.java
+++ Diamond.java
@@ -2,37 +2,34 @@ public class Diamond {
     public static void main(String [] args) {
         Diamond d = new Diamond();
         d.drawDiamond(5);
+        //d.drawIsoc(3);
     }
-    public void drawLine(int n){
-
+    public void drawLine(int n, int i){
+        for (int k=1; k<=(n-(i/2)); k++) {
+            System.out.print("_");
+        }
+        for (int j=1; j<=i; j++) {
+            System.out.print("*");
+        }
+        for (int k=1; k<=(n-(i/2)); k++) {
+            System.out.print("_");
+        }
+        System.out.println("");
     }
 
-    public void drawDiamond(int n) {
-
+    public void drawIsoc(int n) {
         for (int i=1; i<(n*2); i+=2) {
-            for (int k=1; k<=(n-(i/2)); k++) {
-                System.out.print("_");
-            }
-            for (int j=1; j<=i; j++) {
-                System.out.print("*");
-            }
-            for (int k=1; k<=(n-(i/2)); k++) {
-                System.out.print("_");
-            }
-            System.out.println("");
+            drawLine(n, i);
         }
-        for (int i=(n*2)-3; i>=1; i-=2) {
-            for (int k=1; k<=(n-(i/2)); k++) {
-                System.out.print("_");
-            }
-            for (int j=1; j<=i; j++) {
-                System.out.print("*");
-            }
-            for (int k=1; k<=(n-(i/2)); k++) {
-                System.out.print("_");
-            }
-            System.out.println("");
-        }
+    }
 
+    public void drawBottom(int n) {
+        for (int i=(n*2)-3; i>=1; i-=2) {
+            drawLine(n, i);
+        }
+    }
+    public void drawDiamond(int n) {
+        drawIsoc(n);
+        drawBottom(n);
         }
 }
