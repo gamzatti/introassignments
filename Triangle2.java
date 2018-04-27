33b86addae4a4b10545ed1d0a4bf3ccdab927d9b
Emily Coats <emilycoats@riseup.net>
2018-04-27 16:15:35 +1000

    Where has my repository gone???

--- Triangle2.java
+++ Triangle2.java
@@ -1,8 +1,9 @@
 public class Triangle2 {
     public static void main (String args[]){
         Triangle2 t = new Triangle2();
-        t.drawHorizontal(8);
-        t.drawVertical(3);
+        //t.drawHorizontal(8);
+        //t.drawVertical(3);
+        t.drawTriangle(3);
     }
 
     public void drawHorizontal(int n) {
@@ -15,6 +16,15 @@ public class Triangle2 {
     public void drawVertical(int n) {
         for (int i=0; i<n; i++) {
             System.out.println("*");
+        }
+    }
+
+    public void drawTriangle(int n) {
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
