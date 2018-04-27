ae200904ad4e1a5826f3377b90e8307ac89a0312
Emily Coats <emilycoats@riseup.net>
2018-04-27 17:31:39 +1000

    FizzBuzz created.

---
+++ FizzBuzz.java
@@ -0,0 +1,21 @@
+public class FizzBuzz {
+    public static void main (String[] args) {
+        FizzBuzz f = new FizzBuzz();
+        f.go();
+    }
+
+    public void go(){
+        for (int i=1; i<=100; i++) {
+            if (i % 3 == 0) {
+                System.out.print("Fizz");
+            }
+            if (i % 5 == 0) {
+                System.out.print("Buzz");
+            }
+            if (i % 3 != 0 && i % 5 != 0) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
