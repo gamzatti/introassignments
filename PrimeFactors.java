07037fa818b5a0a0d650bd6fa6559c7de65690de
Emily Coats <emilycoats@riseup.net>
2018-04-30 22:51:14 +1000

    Make method that checks whether i is a factor of n. Make skeleton method for checking prime. Have generate method call both methods.

--- PrimeFactors.java
+++ PrimeFactors.java
@@ -3,15 +3,30 @@ import java.util.ArrayList;
 public class PrimeFactors {
     public static void main (String [] args) {
         PrimeFactors p = new PrimeFactors();
-        ArrayList<Integer> result = p.generate(1);
-        System.out.println(result);
+        //ArrayList<Integer> result = p.generate(1);
+        //System.out.println(result);
         //result = p.generate(30);
-        //System.out.println(p.generate(1));
+        System.out.println(p.generate(30));
 
     }
 
     private ArrayList<Integer> generate(int n) {
         ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 1; i<n; i++) {
+            if (isfactor(n,i) && isprime(i)) {
+                result.add(i);
+            }
+        }
         return result;
     }
+
+    private boolean isfactor(int n, int i) {
+        return n % i == 0;
+    }
+
+
+    private boolean isprime(int i) {
+        return true;
+    }
+
 }
