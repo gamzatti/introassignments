42938827aca730506f7e528e4c7869f589a09d86
Emily Coats <emilycoats@riseup.net>
2018-04-30 21:30:40 +1000

    Refactor for performance.

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -4,16 +4,17 @@ public class FizzBuzz {
         f.go();
     }
 
-    public void go(){
+    private void go(){
         for (int i=1; i<=100; i++) {
-            if (i % 3 == 0) {
-                System.out.print("Fizz");
-            }
-            if (i % 5 == 0) {
-                System.out.print("Buzz");
-            }
             if (i % 3 != 0 && i % 5 != 0) {
                 System.out.print(i);
+            } else {
+                if (i % 3 == 0) {
+                    System.out.print("Fizz");
+                }
+                if (i % 5 == 0) {
+                    System.out.print("Buzz");
+                }
             }
             System.out.println();
         }
