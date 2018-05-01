97bfeba2a9d839444b6877538250c5419354107c
Emily Coats <emilycoats@riseup.net>
2018-05-02 09:59:39 +1000

    Finish writing isprime method, test against different prime and non-prime numbers, then test generate works as expected.

--- PrimeFactors.java
+++ PrimeFactors.java
@@ -3,10 +3,21 @@ import java.util.ArrayList;
 public class PrimeFactors {
     public static void main (String [] args) {
         PrimeFactors p = new PrimeFactors();
-        //ArrayList<Integer> result = p.generate(1);
-        //System.out.println(result);
-        //result = p.generate(30);
+        System.out.println(p.generate(1));
+        System.out.println(p.generate(5));
         System.out.println(p.generate(30));
+        System.out.println(p.generate(45));
+        System.out.println(p.generate(60));
+//        System.out.println("1 " + p.isprime(1));   //false
+//        System.out.println("2 " + p.isprime(2));   //true
+//        System.out.println("3 " + p.isprime(3));   //true
+//        System.out.println("4 " + p.isprime(4));   //false
+//        System.out.println("5 " + p.isprime(5));   //true
+//        System.out.println("6 " + p.isprime(6));   //false
+//        System.out.println("7 " + p.isprime(7));   //true
+//        System.out.println("8 " + p.isprime(8));   //false
+//        System.out.println("9 " + p.isprime(9));   //false
+//        System.out.println("10 " + p.isprime(10));   //false
 
     }
 
@@ -26,6 +37,14 @@ public class PrimeFactors {
 
 
     private boolean isprime(int i) {
+        if (i == 1) {
+            return false;
+        }
+        for (int j = 2; j < i; j++) {
+            if (isfactor(i,j)) {
+                return false;
+            }
+        }
         return true;
     }
 
